379b3a05121fa5ba4f1d1ad27f3f6bc3afb81a4f
NuvandaPV <jp@hhmn.de>
2022-09-08 03:19:17 +0200

    Add predicate-based View transformer

---
+++ Chronos/Views/View+if.swift
@@ -0,0 +1,36 @@
+//
+//  View+if.swift
+//  Chronos
+//
+//  Created by Jean-Pierre Höhmann on 2022-09-08.
+//
+//
+
+import Foundation
+import SwiftUI
+
+
+// MARK: View
+
+// MARK: + if
+
+extension View {
+
+    // MARK: - Methods
+
+    /// Applies the given transform if the given condition evaluates to true.
+    ///
+    /// - Parameters:
+    ///     - condition: The condition to evaluate
+    ///     - transform: The transform to apply to the source View.
+    /// - Returns: Either the original View or the modified View.
+    ///
+    @ViewBuilder
+    func `if`<Content: View>(_ condition: @autoclosure () -> Bool, transform: (Self) -> Content) -> some View {
+        if condition() {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
