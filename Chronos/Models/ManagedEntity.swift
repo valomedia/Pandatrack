219da633d8883408f6452003a048750b8e81a2a4
NuvandaPV <jp@hhmn.de>
2022-10-27 10:12:11 +0200

    Add ManagedEntity.swift.

---
+++ Chronos/Models/ManagedEntity.swift
@@ -0,0 +1,66 @@
+//
+//  ManagedEntity.swift
+//  Chronos
+//
+//  Created by Jean-Pierre Höhmann on 2022-10-26.
+//
+//
+
+import Foundation
+import CoreData
+import Combine
+
+
+// MARK: ManagedEntity
+
+/// Undocumented.
+///
+/// - Todo: Document.
+///
+@propertyWrapper class ManagedEntity<Wrapped: NSManagedObject & Entity>: ObservableObject {
+
+    // MARK: - Life cycle methods
+
+    /// Undocumented.
+    ///
+    /// - Todo: Document.
+    /// - Parameters:
+    ///     - wrappedValue:
+    ///
+    init(wrappedValue: Wrapped?) {
+        self.wrappedValue = wrappedValue
+    }
+
+    /// Undocumented.
+    ///
+    /// - Todo: Document.
+    /// - Parameters:
+    ///     - wrappedValue:
+    ///
+    convenience init(_ wrappedValue: Wrapped?) {
+        self.init(wrappedValue: wrappedValue)
+    }
+
+    // MARK: - Properties
+
+    /// Undocumented.
+    ///
+    /// - Todo: Document.
+    ///
+    var wrappedValue: Wrapped? {
+        get {
+            _wrappedValue?.primitiveId.flatMap { _ in _wrappedValue }
+        }
+        set {
+            _wrappedValue = newValue
+            anyCancellable?.cancel()
+            anyCancellable = _wrappedValue?.objectWillChange.sink { [weak self] in
+                self?.objectWillChange.send()
+            }
+        }
+    }
+    @Published var _wrappedValue: Wrapped?
+
+    private var anyCancellable: AnyCancellable? = nil
+
+}
