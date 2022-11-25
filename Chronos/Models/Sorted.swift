f022d0702c14382b6898dd788fff480f0a196fa7
NuvandaPV <jp@hhmn.de>
2022-11-25 17:52:06 +0100

    Add @Sorted

--- Chronos/Models/Sorted.swift
+++ Chronos/Models/Sorted.swift
@@ -15,24 +15,59 @@ import Foundation
 ///
 /// - Todo: Document.
 ///
-@propertyWrapper class Sorted<Element: Comparable> {
+@propertyWrapper class Sorted<Element, Key: Comparable>: RandomAccessCollection {
+    typealias Index = AnyRandomAccessCollection<Element>.Index
+    typealias Indices = AnyRandomAccessCollection<Element>.Indices
+    typealias Element = AnyRandomAccessCollection<Element>.Element
+    typealias SubSequence = AnyRandomAccessCollection<Element>.SubSequence
 
     // MARK: - Life cycle methods
 
-    init(wrappedValue: [Element]) {
-        _wrappedValue = wrappedValue
+    init(_ wrappedValue: AnyCollection<Element>? = nil, by keyPath: KeyPath<Element, Key>) {
+        self.wrappedValue = wrappedValue ?? AnyCollection([])
+        self.keyPath = keyPath
     }
 
-    convenience init(_ wrappedValue: [Element]) {
-        self.init(wrappedValue: wrappedValue)
+    convenience init(wrappedValue: AnyCollection<Element>, by keyPath: KeyPath<Element, Key>) {
+        self.init(wrappedValue, by: keyPath)
     }
 
     // MARK: - Properties
 
-    var wrappedValue: [Element] {
-        get { _wrappedValue.sorted() }
-        set { _wrappedValue = newValue }
+    /// Undocumented.
+    ///
+    /// - Todo: Document.
+    ///
+    var keyPath: KeyPath<Element, Key>
+
+    var wrappedValue: AnyCollection<Element>
+
+    var projectedValue: AnyRandomAccessCollection<Element> {
+        AnyRandomAccessCollection(wrappedValue.sorted(by: keyPath))
     }
-    private var _wrappedValue: [Element]
+
+    var indices: Indices { projectedValue.indices }
+    var startIndex: Index { projectedValue.startIndex }
+    var endIndex: Index { projectedValue.endIndex }
+
+    // MARK: - Methods
+
+    subscript(position: Index) -> Element { projectedValue[position] }
+    subscript(bounds: Range<Index>) -> SubSequence { projectedValue[bounds] }
+
+    func index(before i: Index) -> Index { projectedValue.index(before: i) }
+    func index(after i: Index) -> Index { projectedValue.index(after: i) }
+
+}
+
+extension Sorted where Element == Key {
+
+    convenience init(_ wrappedValue: AnyCollection<Element>? = nil) {
+        self.init(wrappedValue, by: \.self)
+    }
+
+    convenience init(wrappedValue: AnyCollection<Element>) {
+        self.init(wrappedValue)
+    }
 
 }
