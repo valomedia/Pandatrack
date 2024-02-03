a071f407c4b18ae7cb2cf1dcda7b7dc1ff1bfd90
INuvanda <jean-pierre@hoehmann.jp>
2024-02-03 20:08:27 +0100

    Make String identifiable.

---
+++ Chronos/Extensions/String+Identifiable.swift
@@ -0,0 +1,23 @@
+//
+//  String+Identifiable.swift
+//  Chronos
+//
+//  Created by Jean-Pierre Höhmann on 2023-01-20.
+//
+//
+
+import Foundation
+
+
+// MARK: String
+
+// MARK: + Identifiable
+
+extension String: Identifiable {
+    public typealias ID = Int
+
+    public var id: Int {
+        hash
+    }
+
+}
